ee7d7777f7dfa9fd39e2789f96503c584fbc7d0d
Matheus Braga <matheus.braga.work@gmail.com>
2020-08-04 23:38:20 -0300

    :construction: Use routes and change to Link.

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import logoSvg from '../../assets/images/logo.svg';
 import landingSvg from '../../assets/images/landing.svg';
 
@@ -25,14 +27,14 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
-          <a href="" className="give-classes">
+          </Link>
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar Aulas"/>
             Dar Aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
